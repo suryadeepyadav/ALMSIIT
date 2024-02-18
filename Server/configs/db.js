9263e79b8c846a57f2d78e04f10c83b5636d6329
suryadeep yadav <95708869+suryadeepyadav@users.noreply.github.com>
2024-02-18 19:43:39 +0530

    Update db.js
    dburl change localu

--- Server/configs/db.js
+++ Server/configs/db.js
@@ -3,5 +3,5 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connection = mongoose.connect(process.env.dbURL);
-// const connection = mongoose.connect("mongodb://localhost:27017/test")
+// const connection = mongoose.connect("mongodb+srv://suryadeepyadav:<surya@1012@>@cluster0.ycu6uss.mongodb.net/?retryWrites=true&w=majority")
 module.exports = { connection };
